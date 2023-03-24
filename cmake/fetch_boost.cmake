3a789cb31dff4d79b0a379a295bf74aff03aebeb
Miakel Penttinen <mikael.penttinen3@gmail.com>
2023-03-24 20:09:06 +0200

    Add fetch boost libraries

---
+++ cmake/fetch_boost.cmake
@@ -0,0 +1,15 @@
+message(STATUS "Fetching boost. DO NOT PANIC! This will take some time!")
+include(FetchContent)
+
+set(BOOST_INCLUDE_LIBRARIES json)
+set(BOOST_ENABLE_CMAKE ON)
+
+include(FetchContent)
+FetchContent_Declare(
+    Boost
+        GIT_REPOSITORY https://github.com/boostorg/boost.git
+        GIT_TAG boost-1.81.0
+        GIT_SHALLOW TRUE
+        GIT_PROGRESS TRUE
+)
+FetchContent_MakeAvailable(Boost)
